96cb60ec20a27fd8a22bb166b9acad3cac183e99
Freshtohome <jksvenki7@gmail.com>
2026-07-13 17:12:47 +0530

    Add card game functionality with random card selection

---
+++ game.js
@@ -0,0 +1,18 @@
+const cards=[
+"A♠","2♠","3♠","4♠","5♠","6♠","7♠","8♠","9♠","10♠","J♠","Q♠","K♠",
+"A♥","2♥","3♥","4♥","5♥","6♥","7♥","8♥","9♥","10♥","J♥","Q♥","K♥",
+"A♦","2♦","3♦","4♦","5♦","6♦","7♦","8♦","9♦","10♦","J♦","Q♦","K♦",
+"A♣","2♣","3♣","4♣","5♣","6♣","7♣","8♣","9♣","10♣","J♣","Q♣","K♣"
+];
+
+function randomCard(){
+return cards[Math.floor(Math.random()*cards.length)];
+}
+
+function startGame(){
+
+document.getElementById("card1").innerHTML=randomCard();
+document.getElementById("card2").innerHTML=randomCard();
+document.getElementById("card3").innerHTML=randomCard();
+
+}
